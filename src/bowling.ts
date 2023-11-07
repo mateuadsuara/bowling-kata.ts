c618c6ea9fd7d11a7add7143d7f20cb1edc714be
Mateu Adsuara <mateuadsuara@gmail.com>
2023-11-07 16:23:04 +0100

    Solution

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,1 +1,34 @@
-export default () => "foo"
+const ALL_PINS = 10
+
+export function score(rolls: number[], frame: number = 1): number {
+    if (rolls.length <= 1) {
+        return rolls[0] || 0
+    }
+
+    const isStrike = rolls[0] == ALL_PINS
+    const isSpare = rolls[0] + rolls[1] == ALL_PINS
+
+    if (frame == 10) {
+        const throwsInFrame = isStrike || isSpare ? 3 : 2
+        const rollsInFrame = rolls.slice(0, throwsInFrame)
+        return rollsInFrame.reduce((acc, n) => acc + n, 0)
+    }
+
+    if (isStrike) {
+        const frameScore = rolls[0]
+        const rest = rolls.slice(1)
+        const firstBonus = rolls[1]
+        const secondBonus = rolls[2] || 0
+        return frameScore + firstBonus + secondBonus + score(rest, frame + 1)
+    }
+
+    const frameScore = rolls[0] + rolls[1]
+    const rest = rolls.slice(2)
+
+    if (isSpare) {
+        const bonus = rolls[2] || 0
+        return frameScore + bonus + score(rest, frame + 1)
+    }
+
+    return frameScore + score(rest, frame + 1)
+}
--- test/bowling.test.ts
+++ test/bowling.test.ts
@@ -1,5 +1,49 @@
-import bowling from '../src/bowling';
+import {score} from '../src/bowling';
 
-test('hi', () => {
-    expect(bowling()).toEqual("hi")
+test('no rolls', () => {
+    expect(score([])).toEqual(0)
+})
+
+test('one roll of 5 pins', () => {
+    expect(score([5])).toEqual(5)
+})
+
+test('two rolls of 5 and 4 pins', () => {
+    expect(score([5, 4])).toEqual(9)
+})
+
+test('two open frames', () => {
+    expect(score([5, 4, 4, 5])).toEqual(18)
+})
+
+test('a spare with an additional roll knocking off 1 pin', () => {
+    expect(score([5, 5, 1])).toEqual(12)
+})
+
+test('many spares and missing bonus', () => {
+    expect(score([6, 4, 4, 6, 7, 3])).toEqual(41)
+})
+
+test('a strike with an additional open frame', () => {
+    expect(score([10, 5, 4])).toEqual(28)
+})
+
+test('many strikes and missing bonus', () => {
+    expect(score([10, 10, 10])).toEqual(60)
+})
+
+test('final frame and complete game', () => {
+    expect(score([5, 4, 4, 6, 7, 0, 10, 10, 10, 5, 3, 6, 4, 4, 6, 10, 10, 10])).toEqual(178)
+})
+
+test('ignores excess rolls in 10th frame if an open frame', () => {
+    expect(score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 5])).toEqual(1)
+})
+
+test('ignores excess rolls in 10th frame if a strike', () => {
+    expect(score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 1, 5])).toEqual(11)
+})
+
+test('ignores excess rolls in 10th frame if a spare', () => {
+    expect(score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 2, 1, 5])).toEqual(11)
 })
